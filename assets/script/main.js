cfc0f5ca5e7ba402b944459d9e592385426cc7f4
Felipe Boufleuher <97628166+FelipeBoufleuher@users.noreply.github.com>
2023-06-15 21:55:48 -0300

    Carrossel update

--- assets/script/main.js
+++ assets/script/main.js
@@ -8,3 +8,8 @@ buttonMenu.addEventListener("click", () => {
         list.classList.add("ativa")
     }
 })
+
+const inputCarrossel1 = document.getElementById("input1")
+const inputCarrossel2 = document.getElementById("input2")
+const inputCarrossel3 = document.getElementById("input3")
+const inputCarrossel4 = document.getElementById("input4")
