eefdb98795abe6046e4ff60211952e07ebe183ca
Edgar Gavioli <58188033+EED-Edgar@users.noreply.github.com>
2023-06-15 21:59:53 -0300

    Add files via upload

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,11 +1,32 @@
 const buttonMenu = document.getElementById("botaoMenu");
+const buttonCloseMenu = document.getElementById("botaoClose");
 const list = document.getElementById("menu");
 const mobile_menu = document.getElementById("mobile_menu")
 
+window.addEventListener("scroll", () => {
+    if(scrollY > 50) {
+        mobile_menu.classList.add("onScroll");
+    }else{
+        mobile_menu.classList.remove("onScroll");
+    }
+})
+
 buttonMenu.addEventListener("click", () => {
     if(list.className == "ativa"){
+        mobile_menu.classList.remove("menu_on");
         list.classList.remove("ativa")
     }else{
+        mobile_menu.classList.add("menu_on");
+        list.classList.add("ativa")
+    }
+})
+
+buttonCloseMenu.addEventListener("click", () => {
+    if(list.className == "ativa"){
+        mobile_menu.classList.remove("menu_on");
+        list.classList.remove("ativa")
+    }else{
+        mobile_menu.classList.add("menu_on");
         list.classList.add("ativa")
     }
 })
